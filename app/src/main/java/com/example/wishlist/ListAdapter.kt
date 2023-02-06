90453edfeab9c7bead215f05b41bb8107f7913e0
Juan Lopez <juanclopez135@gmail.com>
2023-02-06 14:46:48 -0500

    customized and working links, adding remove function next...

--- app/src/main/java/com/example/wishlist/ListAdapter.kt
+++ app/src/main/java/com/example/wishlist/ListAdapter.kt
@@ -1,22 +1,22 @@
 package com.example.wishlist
 
-import android.graphics.Color
-import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import kotlin.collections.List
 
-class ListAdapter (private val items: List<Items>): RecyclerView.Adapter<ListAdapter.ViewHolder>() {
+class ListAdapter(private val items: List<Items>) : RecyclerView.Adapter<ListAdapter.ViewHolder>() {
     private var x: MutableList<Items> = items as MutableList<Items>
+
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         val item: TextView
         val price: TextView
         val link: TextView
+
 
         // We also create a constructor that accepts the entire item row
         // and does the view lookups to find each sub-view
@@ -25,7 +25,11 @@ class ListAdapter (private val items: List<Items>): RecyclerView.Adapter<ListAda
             item = itemView.findViewById(R.id.itemTv)
             price = itemView.findViewById(R.id.priceTv)
             link = itemView.findViewById(R.id.storeLink)
+
+
         }
+
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -37,19 +41,23 @@ class ListAdapter (private val items: List<Items>): RecyclerView.Adapter<ListAda
         // Return a new holder instance
         return ViewHolder(contactView)
     }
+
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         // Get the data model based on position
-        val item = items.get(position)
+        val item = items[position]
+
         // Set item views based on views and data model
         holder.item.text = item.item
-        holder.price.text = item.price.toString()
+        holder.price.text = item.price
         holder.link.text = item.link
-        
     }
+
     override fun getItemCount(): Int {
         return items.size
     }
-    fun deleteItem(index: Int){
+
+    fun deleteItem(index: Int) {
+
         x.removeAt(index)
         notifyDataSetChanged()
     }
--- app/src/main/java/com/example/wishlist/MainActivity.kt
+++ app/src/main/java/com/example/wishlist/MainActivity.kt
@@ -1,17 +1,17 @@
 package com.example.wishlist
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
-import android.text.method.LinkMovementMethod
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.wishlist.ListFetcher.Companion.addItemFunc
-import com.example.wishlist.ListFetcher.Companion.itemsList
-import com.example.wishlist.ListFetcher.Companion.link
+import com.example.wishlist.ListFetcher.Companion.getItems
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,14 +20,14 @@ class MainActivity : AppCompatActivity() {
 
         val itemsRv = findViewById<RecyclerView>(R.id.listRv)
 
-        val items = ListFetcher.getItems()
+        val items = getItems()
         val adapter = ListAdapter(items)
 
         itemsRv.adapter = adapter
 
         itemsRv.layoutManager = LinearLayoutManager(this)
 
-        findViewById<Button>(R.id.submitBtn).setOnClickListener{
+        findViewById<Button>(R.id.submitBtn).setOnClickListener {
             val name = findViewById<EditText>(R.id.itemName)
             val price = findViewById<EditText>(R.id.price)
             val link2 = findViewById<EditText>(R.id.store)
@@ -36,19 +36,34 @@ class MainActivity : AppCompatActivity() {
             val priceText = price.text.toString()
             val linkText = link2.text.toString()
 
+            name.hideKeyboard()
+
             val add = addItemFunc(nameText, priceText, linkText)
             items.add(add)
 
             Toast.makeText(this, "Item Added to List!", Toast.LENGTH_SHORT).show()
 
+
+            name.text.clear()
+            price.text.clear()
+            link2.text.clear()
+            name.hint = "Item Name"
+            price.hint = "Ex: 10.99"
+            link2.hint = "Ex: www.google.com"
+
             adapter.notifyDataSetChanged()
 
         }
 
+        itemsRv.setOnClickListener {
+            Toast.makeText(this, "Item Number: $adapter", Toast.LENGTH_SHORT).show()
 
+        }
     }
-    fun removeItem(position: Int){
-        val x = ListFetcher.getItems()
-        x.removeAt(position)
+
+    private fun View.hideKeyboard() {
+        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(windowToken, 0)
     }
+
 }
